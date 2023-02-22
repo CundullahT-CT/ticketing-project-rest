51eaf89235a6795a441ed17e4e6a8c0fe69299a4
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-02-22 22:13:26 +0100

    delete tests

--- src/test/java/com/cydeo/unit_test_review/UserServiceUnitTest.java
+++ src/test/java/com/cydeo/unit_test_review/UserServiceUnitTest.java
@@ -1,9 +1,11 @@
 package com.cydeo.unit_test_review;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Role;
 import com.cydeo.entity.User;
+import com.cydeo.exception.TicketingProjectException;
 import com.cydeo.mapper.UserMapper;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.KeycloakService;
@@ -13,6 +15,8 @@ import com.cydeo.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -20,6 +24,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -134,7 +139,11 @@ public class UserServiceUnitTest {
         assertInstanceOf(NoSuchElementException.class, throwable);
         assertEquals("User not found", throwable.getMessage());
 
-//        assertThrowsExactly(NoSuchElementException.class, () -> userService.findByUserName("SomeUsername"), "User not found");
+//        assertThrows(NoSuchElementException.class, () -> userService.findByUserName("SomeUsername"));
+//        assertEquals("User not found", throwable.getMessage());
+//
+//        assertThrowsExactly(NoSuchElementException.class, () -> userService.findByUserName("SomeUsername"));
+//        assertEquals("User not found", throwable.getMessage());
 
     }
 
@@ -166,13 +175,92 @@ public class UserServiceUnitTest {
 
     }
 
+    @Test
+    void should_delete_manager() throws TicketingProjectException {
 
+        User managerUser = getUser("Manager");
 
+        when(userRepository.findByUserNameAndIsDeleted(anyString(), anyBoolean())).thenReturn(managerUser);
 
+        when(userRepository.save(any())).thenReturn(managerUser);
 
+        when(projectService.listAllNonCompletedByAssignedManager(any())).thenReturn(new ArrayList<>());
 
+        userService.delete(userDTO.getUserName());
 
+        assertTrue(managerUser.getIsDeleted());
+        assertNotEquals("user3", managerUser.getUserName());
 
+    }
 
+    @Test
+    void should_delete_employee() throws TicketingProjectException {
+
+        User employeeUser = getUser("Employee");
+
+        when(userRepository.findByUserNameAndIsDeleted(anyString(), anyBoolean())).thenReturn(employeeUser);
+
+        when(userRepository.save(any())).thenReturn(employeeUser);
+
+        when(taskService.listAllNonCompletedByAssignedEmployee(any())).thenReturn(new ArrayList<>());
+
+        userService.delete(userDTO.getUserName());
+
+        assertTrue(employeeUser.getIsDeleted());
+        assertNotEquals("user3", employeeUser.getUserName());
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Manager", "Employee"})
+    void should_delete_user(String value) throws TicketingProjectException {
+
+        User testUser = getUser(value);
+
+        when(userRepository.findByUserNameAndIsDeleted(anyString(), anyBoolean())).thenReturn(testUser);
+
+        when(userRepository.save(any())).thenReturn(testUser);
+
+        lenient().when(projectService.listAllNonCompletedByAssignedManager(any())).thenReturn(new ArrayList<>());
+
+        lenient().when(taskService.listAllNonCompletedByAssignedEmployee(any())).thenReturn(new ArrayList<>());
+
+        userService.delete(userDTO.getUserName());
+
+        assertTrue(testUser.getIsDeleted());
+        assertNotEquals("user3", testUser.getUserName());
+
+    }
+
+    @Test
+    void should_throw_exception_when_delete_manager() throws TicketingProjectException {
+
+        User managerUser = getUser("Manager");
+
+        when(userRepository.findByUserNameAndIsDeleted(anyString(), anyBoolean())).thenReturn(managerUser);
+
+        when(projectService.listAllNonCompletedByAssignedManager(any())).thenReturn(List.of(new ProjectDTO(), new ProjectDTO()));
+
+        Throwable throwable = catchThrowable(() -> userService.delete(userDTO.getUserName()));
+
+        assertInstanceOf(TicketingProjectException.class, throwable);
+        assertEquals("User can not be deleted", throwable.getMessage());
+
+        verify(userMapper, atLeastOnce()).convertToDto(any());
+
+    }
+
+    private User getUser(String role) {
+
+        User user3 = new User();
+
+        user3.setUserName("user3");
+        user3.setEnabled(false);
+        user3.setIsDeleted(false);
+        user3.setRole(new Role(role));
+
+        return user3;
+
+    }
 
 }
